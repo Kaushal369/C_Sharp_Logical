e27b9f323a19019f420806c90e888b2076723835
Kaush <Kaush@DESKTOP-EEONS1N>
2021-08-20 09:37:41 +0530

    Add project files.

---
+++ webApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace webApp
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    string a = "a1b2c3d4";
+        //    string b = "";
+        //    int val = 0;
+
+        //    for (int i = 0; i < a.Length; i++)
+        //    {
+        //        if (Char.IsDigit(a[i]))
+        //            b += a[i];
+        //    }
+
+        //    if (b.Length > 0)
+        //         val = Int32.Parse(b);
+        //    Console.WriteLine(Convert.ToString(val));
+        //}
+    }
+}
---
+++ webApp/arrayExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace webApp
+{
+    class arrayExample
+    {
+
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[10];
+            int i;
+            for (i = 0; i <arr.Length; i++)
+            {
+                arr[i] = i + 1;
+                Console.WriteLine(arr[i]);
+            }
+            
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
